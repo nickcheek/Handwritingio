d36416e1cbb93e450cecf2fb79f281abe231fd46
Nick <nick@nicholascheek.com>
2019-12-18 06:58:50 -0600

    testing

--- src/Handwriting.php
+++ src/Handwriting.php
@@ -2,7 +2,23 @@
 
 namespace Nickcheek\Handwriting;
 
+use GuzzleHttp\Client;
+
 class Handwriting
 {
-    // Build your next great package.
+    protected string $key;
+    protected string $secret;
+    protected Client $client;
+
+    public function __construct($key, $secret)
+    {
+        $this->key = $key;
+        $this->secret = $secret;
+        $this->client = new Client(['base_uri' => 'https://api.handwriting.io/']);
+    }
+
+    public function getHandwriting()
+    {
+        $response = $this->client->request('GET', 'handwriting');
+    }
 }
